f9b13f6a42b6df02ca2e5c9f169b8d067e95ad29
theDanielWhite <daniel@jwgw.net>
2022-04-01 15:11:21 -0400

    Initial setup for Problem Set 6: Problem 2- Matrix Class

---
+++ matrix.py
@@ -0,0 +1,29 @@
+class Matrix:
+    def __init__(self, matrix_elements):
+        self.matrix_elements = matrix_elements
+        self.dim = ()
+
+    def transpose(self):
+
+    def det(self):
+
+    def is_matrix(A):
+
+    def sort(self, type):
+
+    def save(self, filename):
+
+    def load(self, filename):
+
+    def __add__(self, other):
+
+    def __sub__(self, other):
+
+    def __mul__(self, other):
+
+    def __eq__(self, other):
+
+    def __len__(self):
+
+    def __str__(self):
+        return f''
